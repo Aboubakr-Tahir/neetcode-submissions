39442600c721b2e7780895e066655804710ff313
Aboubakr Tahir <aboubakrtahir44@gmail.com>
2026-07-15 10:25:05 +0100

    Add: is-palindrome - submission-10

---
+++ Data Structures & Algorithms/is-palindrome/submission-10.py
@@ -0,0 +1,20 @@
+import re 
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l = 0 
+        r = len(s) - 1
+        while l < r :
+            if not s[r].isalnum() :
+                r-= 1
+                continue
+            if not s[l].isalnum() :
+                l+=1
+                continue
+            if s[l].lower() == s[r].lower() :
+                r-= 1
+                l+= 1
+                continue
+            if s[l].lower() != s[r].lower() : 
+                return False
+        return True 
